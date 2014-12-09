80d7b0332619d69610d6bd0bce740419b2f3600c
csljxing <csljxing@gmail.com>
2014-12-09 13:12:12 +0800

    Use Double Matrix

--- template_match.m
+++ template_match.m
@@ -1,29 +1,24 @@
 function template_match
 
     vidPath = '../video/01_david/david.avi';
-    outPath = '../video/01_david/out';
+    outPath = '../video/01_david/out2';
 
     % Load video
     vid = VideoReader( vidPath);
-    origVidFrames = vid.read();
-    vidFrames = 0.299*origVidFrames( :, :, 1, :) + 0.587*origVidFrames( :, :, 2, :) ...
-                + 0.114*origVidFrames( :, :, 3, :);
-    sz = size( vidFrames);
-    vidFrames = reshape( vidFrames, sz(1), sz(2), sz(4));
-    
     
     %Global variable
     global numberOfFrames numberOfRows numberOfCols numberOfRowsTem numberOfColsTem
-    numberOfFrames = sz(4);
-    numberOfRows = sz(1);
-    numberOfCols = sz(2);
+    numberOfFrames = vid.numberOfFrames;
+    numberOfRows = vid.height;
+    numberOfCols = vid.width;
 
     % Get model 
-    frameOne = vidFrames( :, :, 1);
+    frameOne = vid.read( 1);
     imshow( frameOne);
     % xmin ymin width height]
     pos = getPosition( imrect);
-    model = frameOne( round( pos(2) : pos(2)+pos(4)), round( pos(1) : pos(1)+pos(3)), :);
+    grayFrameOne = im2double( rgb2gray( frameOne));
+    model = grayFrameOne( round( pos(2) : pos(2)+pos(4)), round( pos(1) : pos(1)+pos(3)), :);
     [ numberOfRowsTem, numberOfColsTem,] = size( model);
     
     %Save output video
@@ -34,17 +29,18 @@ function template_match
     for k = 2 : 1 : numberOfFrames
         row = round( pos(2));
         col = round( pos(1));
-        grayFrames = vidFrames( :, :, k);
-        [ model, row, col] = tracking( grayFrames, model, row, col);
-        origVidFrames( [ row, row+numberOfRowsTem-1], col : col+numberOfColsTem-1, 1, k) = 255;
-        origVidFrames( [ row, row+numberOfRowsTem-1], col : col+numberOfColsTem-1, 2:3, k) = 0;
-        origVidFrames( row : row+numberOfRowsTem-1, [ col, col+numberOfColsTem-1], 1, k) = 255;
-        origVidFrames( row : row+numberOfRowsTem-1, [ col, col+numberOfColsTem-1], 2:3, k) = 0;
-        disp( k);
+        origFrame = vid.read( k);
+        grayFrame = im2double( rgb2gray( origFrame));
+        [ model, bestSimilarity, row, col] = tracking( grayFrame, model, row, col);
+        origFrame( [ row, row+numberOfRowsTem-1], col : col+numberOfColsTem-1, 1) = 255;
+        origFrame( [ row, row+numberOfRowsTem-1], col : col+numberOfColsTem-1, 2:3) = 0;
+        origFrame( row : row+numberOfRowsTem-1, [ col, col+numberOfColsTem-1], 1) = 255;
+        origFrame( row : row+numberOfRowsTem-1, [ col, col+numberOfColsTem-1], 2:3) = 0;
+        disp( [ 'Frame ' num2str( k) ' : ' num2str( bestSimilarity)]);
         %Show directly
-        imshow( origVidFrames( :, :, :, k));
+        imshow( origFrame);
         %Save to output video
-        vidWriter.writeVideo( origVidFrames( :, :, :, k));
+        vidWriter.writeVideo( origFrame);
     end
 
     % Save output video
@@ -53,7 +49,7 @@ function template_match
 end %template_match
 
 %---------------------------------------------------------------------
-function [ model, row, col] = tracking( frame, model, rowStart, colStart)
+function [ model, bestSimilarity, row, col] = tracking( frame, model, rowStart, colStart)
     %Global variable
     global numberOfRows numberOfCols numberOfRowsTem numberOfColsTem;
     rowStart = round( rowStart - numberOfRowsTem);
@@ -65,49 +61,48 @@ function [ model, row, col] = tracking( frame, model, rowStart, colStart)
         colStart = 1;
     end
     
-    %% ---------------------------------------------------------------------------
-    rowEnd = rowStart + 3*numberOfRowsTem;
-    if rowEnd > numberOfRows
-        rowEnd = numberOfRows;
-    end
-    colEnd = colStart + 3*numberOfColsTem;
-    if colEnd > numberOfCols
-        colEnd = numberOfCols;
-    end
-    
-    [ bestSimilarity, row, col] = matchTemplate( frame, model, [ rowStart, rowEnd, colStart, colEnd]);
-    row = round( row);
-    col = round( col);
-    
-    %% ---------------------------------------------------------------------------
-%     rowEnd = rowStart + 2*numberOfRowsTem;
-%     if rowEnd > ( numberOfRows - numberOfRowsTem + 1)
-%         rowEnd = numberOfRows - numberOfRowsTem + 1;
+%     %% ---------------------------------------------------------------------------
+%     rowEnd = rowStart + 3*numberOfRowsTem;
+%     if rowEnd > numberOfRows
+%         rowEnd = numberOfRows;
 %     end
-%     colEnd = colStart + 2*numberOfColsTem;
-%     if colEnd > ( numberOfCols - numberOfColsTem + 1)
-%         colEnd = numberOfCols - numberOfColsTem + 1;
+%     colEnd = colStart + 3*numberOfColsTem;
+%     if colEnd > numberOfCols
+%         colEnd = numberOfCols;
 %     end
 %     
-%     row = rowStart;
-%     col = colStart;
-%     bestSimilarity = -Inf;
-%     
-%     for i = rowStart : 2 : rowEnd
-%         for j = colStart : 2 : colEnd
-%             similarity = getSimilarity( frame, model, i, j);
-%             if similarity > bestSimilarity
-%                 bestSimilarity = similarity;
-%                 row = i;
-%                 col = j;
-%             end
-%         end
-%     end
+%     [ bestSimilarity, row, col] = matchTemplate( frame, model, [ rowStart, rowEnd, colStart, colEnd]);
+%     row = round( row);
+%     col = round( col);
+%     %% ---------------------------------------------------------------------------
+    
+    rowEnd = rowStart + 2*numberOfRowsTem;
+    if rowEnd > ( numberOfRows - numberOfRowsTem + 1)
+        rowEnd = numberOfRows - numberOfRowsTem + 1;
+    end
+    colEnd = colStart + 2*numberOfColsTem;
+    if colEnd > ( numberOfCols - numberOfColsTem + 1)
+        colEnd = numberOfCols - numberOfColsTem + 1;
+    end
+    
+    row = rowStart;
+    col = colStart;
+    bestSimilarity = -Inf;
+    
+    for i = rowStart : 2 : rowEnd
+        for j = colStart : 2 : colEnd
+            similarity = getSimilarity( frame, model, i, j);
+            if similarity > bestSimilarity
+                bestSimilarity = similarity;
+                row = i;
+                col = j;
+            end
+        end
+    end
     
 %     model = frame( row : row + numberOfRowsTem - 1,...
-                        col : col + numberOfColsTem -1);
-    disp( bestSimilarity);
-    if bestSimilarity > 0.2
+%                         col : col + numberOfColsTem -1);  
+    if bestSimilarity > 0.8
         model = frame( row : row + numberOfRowsTem - 1,...
                         col : col + numberOfColsTem -1);
     else
