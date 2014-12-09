54c79154c30cab8942bad5233254a73d88bf623e
csljxing <csljxing@gmail.com>
2014-12-09 15:10:48 +0800

    Finish mex( Matlab, OpenCv) code template matching tracking

--- template_match.m
+++ template_match.m
@@ -19,7 +19,7 @@ function template_match
     imshow( frameOne);
     % xmin ymin width height]
     pos = getPosition( imrect);
-    grayFrameOne = im2double( rgb2gray( frameOne));
+    grayFrameOne = rgb2gray( frameOne);
     model = grayFrameOne( round( pos(2) : pos(2)+pos(4)), round( pos(1) : pos(1)+pos(3)), :);
     [ numberOfRowsTem, numberOfColsTem,] = size( model);
     
@@ -32,7 +32,7 @@ function template_match
         row = round( pos(2));
         col = round( pos(1));
         origFrame = origVidFrames( :, :, :, k);
-        grayFrame = im2double( rgb2gray( origFrame));
+        grayFrame = rgb2gray( origFrame);
         [ model, bestSimilarity, row, col] = tracking( grayFrame, model, row, col);
         origFrame( [ row, row+numberOfRowsTem-1], col : col+numberOfColsTem-1, 1) = 255;
         origFrame( [ row, row+numberOfRowsTem-1], col : col+numberOfColsTem-1, 2:3) = 0;
@@ -54,41 +54,12 @@ end %template_match
 function [ model, bestSimilarity, row, col] = tracking( frame, model, rowStart, colStart)
 
     %% Global variable
-    global numberOfRows numberOfCols numberOfRowsTem numberOfColsTem;
+    global numberOfRowsTem numberOfColsTem;
     %% End Global variable
     
-    rowStart = round( rowStart - numberOfRowsTem);
-    colStart = round( colStart - numberOfColsTem);
-    rowEnd = rowStart + 2*numberOfRowsTem;
-    colEnd = colStart + 2*numberOfColsTem;
-    
-    if rowStart < 1
-        rowStart = 1;
-    end
-    if colStart  < 1
-        colStart = 1;
-    end
-    if rowEnd > ( numberOfRows - numberOfRowsTem + 1)
-        rowEnd = numberOfRows - numberOfRowsTem + 1;
-    end
-    if colEnd > ( numberOfCols - numberOfColsTem + 1)
-        colEnd = numberOfCols - numberOfColsTem + 1;
-    end
-    
-    row = rowStart;
-    col = colStart;
-    bestSimilarity = -Inf;
-    
-    for i = rowStart : 2 : rowEnd
-        for j = colStart : 2 : colEnd
-            similarity = getSimilarity( frame, model, i, j);
-            if similarity > bestSimilarity
-                bestSimilarity = similarity;
-                row = i;
-                col = j;
-            end
-        end
-    end
+    [ bestSimilarity, row, col] = matchTemplate( frame, model, [ rowStart, colStart]);
+    row = round( row);
+    col = round( col);
     
     if bestSimilarity > 0.8
         model = frame( row : row + numberOfRowsTem - 1,...
@@ -99,25 +70,3 @@ function [ model, bestSimilarity, row, col] = tracking( frame, model, rowStart, 
     end
 
 end % tracking
-
-
-function similarity = getSimilarity( frame, model, rowStart, colStart)
-
-    %% Global variable
-    global numberOfRowsTem numberOfColsTem;
-    %% End Global variable
-    
-%     similarity = sum( sum( ( frame( rowStart : ( rowStart + numberOfRowsTem -1), ...
-%                                 colStart : ( colStart + numberOfColsTem - 1))...
-%                         - model) .^2));
-   
-    matchWind = frame( rowStart : (rowStart + numberOfRowsTem -1), ...
-                                 colStart : (colStart + numberOfColsTem - 1));
-    matchWind = matchWind - mean2( matchWind);
-    model  = model - mean2( model);
-    similarity = sum( sum( matchWind .* model)) / ...
-                    sqrt( sum( sum( matchWind .^ 2)) * sum( sum( model .^ 2)));
-
-end % getSimilarity
-
-
